f50a87d388c49eaa5cb7add02a7bf5fcb388cd0d
Amir <au.amirutley@gmail.com>
2022-02-17 11:11:40 -0500

    bottomNavView error resolved, some un-needed files deleted

--- app/src/main/java/com/example/nextercise/HomeActivity.java
+++ app/src/main/java/com/example/nextercise/HomeActivity.java
@@ -1,10 +1,8 @@
 package com.example.nextercise;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -19,40 +17,38 @@ public class HomeActivity extends AppCompatActivity {
     final FragmentManager fragmentManager = getSupportFragmentManager();
     private BottomNavigationView bottomNavigationView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reccomend);
+        setContentView(R.layout.activity_home);
         //handle navigation selection
 
-        bottomNavigationView = findViewById(R.id.bottomNavigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                Fragment fragment;
-                switch (menuItem.getItemId()) {
-                    case R.id.action_home:
-                        Toast.makeText(HomeActivity.this, "Home!", Toast.LENGTH_SHORT).show();
-                        fragment = new HomeFragment();
-                        break;
-                    case R.id.action_search:
-                        Toast.makeText(HomeActivity.this, "Search!", Toast.LENGTH_SHORT).show();
-                        fragment = new HomeFragment();
-                        break;
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(menuItem -> {
+            Fragment fragment;
+            switch (menuItem.getItemId()) {
+                case R.id.navigation_search:
+                    Toast.makeText(HomeActivity.this, "Search!", Toast.LENGTH_SHORT).show();
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.navigation_profile:
+                    Toast.makeText(HomeActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
+                    fragment = new HomeFragment();
+                    break;
 
-                    case R.id.action_profile:
-                    default:
-                        // TODO: update fragment
-                        Toast.makeText(HomeActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
-                        fragment = new HomeFragment();
-                        break;
-                }
-                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
-                return true;
+                case R.id.navigation_recommend:
+                default:
+
+                    Toast.makeText(HomeActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+                    fragment = new HomeFragment();
+                    break;
             }
+            fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+            return true;
         });
         // Set default selection
-        bottomNavigationView.setSelectedItemId(R.id.action_home);
+        bottomNavigationView.setSelectedItemId(R.id.navigation_recommend);
 
     }
 }
---
+++ app/src/main/java/com/example/nextercise/RecommendFragment.java
@@ -0,0 +1,31 @@
+package com.example.nextercise;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class RecommendFragment extends Fragment {
+    public static final String TAG = "ExerciseActivity";
+    public RecommendFragment() {
+    }
+    private TextView etExerciseName;
+    private ImageView ivExerciseImage;
+    private TextView etExerciseDescription;
+    private TextView etExerciseInstructions;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(activity_exercise);
+//
+//
+//
+//        etExerciseName = findViewById(R.id.etExerciseName);
+//        ivExerciseImage = findViewById(R.id.ivExerciseImage);
+//        etExerciseDescription = findViewById(R.id.etExcDescription);
+//        etExerciseInstructions = findViewById(R.id.etExerciseInstructions);
+
+    }
+}
