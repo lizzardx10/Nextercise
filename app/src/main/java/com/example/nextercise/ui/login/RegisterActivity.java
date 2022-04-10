040bacb870e966e3e0972bf604166d1d840b7ced
lizzardx10 <au.amirutley@gmail.com>
2022-04-10 14:39:25 -0500

    Made last-minute adjustments to the RegisterActivity so that it works more effectively.

--- app/src/main/java/com/example/nextercise/ui/login/RegisterActivity.java
+++ app/src/main/java/com/example/nextercise/ui/login/RegisterActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.nextercise.R;
+import com.example.nextercise.ui.HomeActivity;
 import com.example.nextercise.ui.MainActivity;
 import com.parse.ParseUser;
 
@@ -33,47 +34,40 @@ public class RegisterActivity extends AppCompatActivity {
         EditText etConfirmPassword = findViewById(R.id.etConfirmPassword);
         Button btnRegister = findViewById(R.id.btnRegister);
 
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "onClick register button");
+        btnRegister.setOnClickListener(v -> {
+            Log.i(TAG, "onClick register button");
+            if(etPassword.getText().toString().equals(etConfirmPassword.getText().toString())) {
                 ParseUser user = new ParseUser();
                 // Set the user's username and password, which can be obtained by a forms
                 user.setUsername( etUsername.getText().toString());
-                //  if(etPassword.getText().toString().equals(etConfirmPassword.getText().toString())) {
                 user.setPassword(etPassword.getText().toString());
                 user.signUpInBackground(e -> {
                     if (e == null) {
                         showAlert("Welcome " + etUsername.getText().toString() + "!");
-                        goLoginActivity();
                     } else {
-                        ParseUser.logOut();
+//                            ParseUser.logOut();
                         Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
                     }
                 });
             }
+            else {
+                Toast.makeText(RegisterActivity.this, "Passwords do not match!!!", Toast.LENGTH_LONG).show();
+            }
         });
-
-           // }
         }
 
     private void showAlert(String message){
         AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this)
-                .setTitle("Successful Sign Up!")
+                .setTitle("Registration Successful!")
                 .setMessage(message)
                 .setPositiveButton("OK", (dialog, which) -> {
                     dialog.cancel();
                     // don't forget to change the line below with the names of your Activities
-                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                    Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                     startActivity(intent);
                 });
         AlertDialog ok = builder.create();
         ok.show();
     }
-    private void goLoginActivity() {
-        Intent i = new Intent(this, LoginActivity.class);
-        startActivity(i);
-    }
-
 }
